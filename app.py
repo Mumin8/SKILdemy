8bff58a2cd4287410e64428e7e374fa789cf8873
Mumin8 <alhassanmumin8@gmail.com>
2024-04-12 17:41:49 +0000

    entry point

---
+++ app.py
@@ -0,0 +1,9 @@
+from learning_platform import app
+from learning_platform.views.Home import home_bp
+from learning_platform.views.users import user_bp
+
+app.register_blueprint(home_bp)
+app.register_blueprint(user_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)

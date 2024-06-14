53d124728871a84474188d72aaf5cf15a3aff337
Mumin8 <alhassanmumin8@gmail.com>
2024-06-14 08:30:14 +0000

    changes

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-
 from learning_platform import app
 from learning_platform.views.home import home_bp
 from learning_platform.views.users import user_bp
 from learning_platform.views.admin import admin_bp
 
-
-
 app.register_blueprint(admin_bp)
 app.register_blueprint(home_bp)
 app.register_blueprint(user_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
